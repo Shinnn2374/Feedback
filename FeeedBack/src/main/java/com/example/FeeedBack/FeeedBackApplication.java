1242cd84451dfc37ed84914586fc2664f031b54d
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-26 19:23:16 +0300

    ...

---
+++ FeeedBack/src/main/java/com/example/FeeedBack/FeeedBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.FeeedBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeedBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeeedBackApplication.class, args);
+	}
+
+}
---
+++ FeeedBack/src/test/java/com/example/FeeedBack/FeeedBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FeeedBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeeedBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

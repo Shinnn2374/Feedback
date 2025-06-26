187c69168930a68268eec53f12af6e906cedc93a
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-26 19:28:13 +0300

    ...

---
+++ FeeedBack/src/main/java/com/example/FeeedBack/entity/RoleType.java
@@ -0,0 +1,6 @@
+package com.example.FeeedBack.entity;
+
+public enum RoleType {
+    ROLE_ADMIN,
+    ROLE_USER
+}
---
+++ FeeedBack/src/main/java/com/example/FeeedBack/entity/User.java
@@ -0,0 +1,33 @@
+package com.example.FeeedBack.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "app_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.ALL)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @Builder.Default
+    private List<Role> roles = new ArrayList<>();
+
+}

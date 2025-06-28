b6a40521996525340ba8625469502606eec6806f
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-28 15:15:38 +0300

    ...

--- FeeedBack/src/main/java/com/example/FeeedBack/service/AuthService.java
+++ FeeedBack/src/main/java/com/example/FeeedBack/service/AuthService.java
@@ -1,4 +1,53 @@
 package com.example.FeeedBack.service;
 
+import com.example.FeeedBack.dto.AuthResponse;
+import com.example.FeeedBack.dto.RegisterRequest;
+import com.example.FeeedBack.model.User;
+import com.example.FeeedBack.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.text.MessageFormat;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse register(RegisterRequest registerRequest) {
+        var user = User.builder()
+                .email(registerRequest.getEmail())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .username(registerRequest.getUsername())
+                .role(registerRequest.getRole())
+                .build();
+
+        userRepository.save(user);
+
+        return AuthResponse.builder()
+                .email(user.getEmail())
+                .username(user.getUsername())
+                .role(user.getRole())
+                .build();
+    }
+
+    public AuthResponse authenticate(String email, String password) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, password)
+        );
+
+        var user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException(MessageFormat.format("User with email - {0} not found", email)));
+
+        return AuthResponse.builder()
+                .email(user.getEmail())
+                .username(user.getUsername())
+                .role(user.getRole())
+                .build();
+    }
 }
